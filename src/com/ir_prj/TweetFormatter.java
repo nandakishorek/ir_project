f101d3251f46231adb62d068a91e0f9f29e557cf
Nandakishore K <nandakishorek17@gmail.com>
2015-09-16 11:17:35 -0400

    Added code for simple formatting

--- src/com/ir_prj/TweetFormatter.java
+++ src/com/ir_prj/TweetFormatter.java
@@ -7,9 +7,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import twitter4j.HashtagEntity;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+import twitter4j.URLEntity;
 
 /**
  * Creates JSON array of tweets from tweets separated by line breaks
+ * 
+ * 
+ * TODO: Not using String buffer. If this turns out to be slow, then use it.
  * 
  * @author kishore
  *
@@ -20,7 +31,8 @@ public class TweetFormatter {
 
         File tweetsDir = new File("tweets");
         for (File tweetFile : tweetsDir.listFiles()) {
-            File formattedTweetFile = new File("formatted" + File.separator + "formatted_" + tweetFile.getName());
+            String fileName = "formatted" + File.separator + "formatted_" + tweetFile.getName();
+            File formattedTweetFile = new File(fileName);
             formattedTweetFile.getParentFile().mkdirs();
 
             // create JSON array of the tweets in this file
@@ -34,7 +46,68 @@ public class TweetFormatter {
                     if (!isStart) {
                         bw.write(",");
                     }
-                    bw.write(tweet);
+                    try {
+                        // tweet start
+                        bw.write("{");
+                        Status status = TwitterObjectFactory.createStatus(tweet);
+                        
+                        // id
+                        bw.write("\"id\":" + status.getId());
+                        
+                        // lang
+                        bw.write(",\"lang\":\"" + status.getLang() + "\"");
+                        
+                        // text
+                        // sanitize the text
+                        String text = status.getText();
+                        text = text.replace('"', '\''); // replace double quotes by single quotes
+                        text = text.replace('\n', ' '); // replace '\n' by space
+                        if (fileName.contains("en")) {
+                            bw.write(",\"tw_text_en\":\"");
+                        } else if (fileName.contains("de")) {
+                            bw.write(",\"tw_text_de\":\"");
+                        } else {
+                            bw.write(",\"tw_text_ru\":\"");
+                        }
+                        
+                        bw.write(text + "\"");
+                        
+                        // timestamp
+                        bw.write(",\"created_at\":\"" + formatDate(status.getCreatedAt()) + "\"");
+                        
+                        // urls
+                        bw.write(",\"urls\":[");
+                        int i = 0;
+                        for (URLEntity ue : status.getURLEntities()) {
+                            if (i != 0) {
+                                bw.write(",");
+                            }
+                            bw.write("\"" + ue.getURL() + "\"");
+                            ++i;
+                        }
+                        bw.write("]");
+                        
+                        // hashtags
+                        bw.write(",\"hashtags\":[");
+                        i = 0;
+                        for (HashtagEntity he : status.getHashtagEntities()) {
+                            if (i != 0) {
+                                bw.write(",");
+                            }
+                            bw.write("\"" + he.getText() + "\"");
+                            ++i;
+                        }
+                        bw.write("]");
+                        
+                        // user name
+                        bw.write(",\"user_name\":\"" + status.getUser().getName() + "\"");
+                        
+                        // tweet end
+                        bw.write("}");
+                    } catch (TwitterException e) {
+                        e.printStackTrace();
+                    }                    
+                    /*bw.write(tweet);*/
                     isStart = false;
                 }
                 
@@ -46,6 +119,13 @@ public class TweetFormatter {
             }
         }
 
+    }
+    
+    
+    public static String formatDate(Date date){
+        String pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        return sdf.format(date);
     }
 
 }
