6b4549d539ae93d186283cea06d74009e7d459a3
Nandakishore K <nandakishorek17@gmail.com>
2015-11-07 22:24:23 -0500

    added query lang detection and basic auth

--- trec/json_to_trec_custom.py
+++ trec/json_to_trec_custom.py
@@ -4,20 +4,24 @@ Created on Tue Oct 27 17:47:48 2015
 
 @author: ruhansa
 
-added functionality to retrieve results for all queries in queries.txt
+1. added functionality to retrieve results for all queries in queries.txt
+2. added query language detection
+3. added basic auth
 """
 import json
 # if you are using python 3, you should 
 # import urllib.request 
 import urllib
 import urllib2
-
+from langdetect import detect_langs
 import argparse
-
+import base64
 
 parser = argparse.ArgumentParser(description="Converts Solr search result from json to trec eval format.")
 parser.add_argument("query_file", help="absolute path to the query file", type=str)
 parser.add_argument("model_name", help="name of the model", type=str)
+parser.add_argument("user_name", help="solr username", type=str)
+parser.add_argument("password", help="solr password", type=str)
 args = parser.parse_args()
 
 # change the url according to your own koding username and query
@@ -25,18 +29,32 @@ inUrlBeg = "http://nandakishorek.koding.io:8983/solr/vsm_core/select?q="
 inUrlEnd = "&fl=id%2Cscore%2Ctext_en%2Ctext_de%2Ctext_ru&wt=json&indent=true&defType=dismax&qf=text_en+text_de+text_ru&mm=1&rows=4000"
 
 
+# init the auth handler
+auth_handler = urllib2.HTTPBasicAuthHandler()
+auth_handler.add_password(realm='Auth',
+                      uri='http://nandakishorek.koding.io:8983/',
+                      user=args.user_name,
+                      passwd=args.password)
+opener = urllib2.build_opener(auth_handler)
+urllib2.install_opener(opener)
+
 queryFile = open(args.query_file, "r");
 for line in queryFile:
     
     indexOfSpace = line.index(" ")
     qid = line[0:indexOfSpace]
-    query = urllib.quote(line[indexOfSpace+1:-1])
+    query = line[indexOfSpace+1:-1]
+
+    langs = detect_langs(query.decode("UTF-8"))
+
+    print langs
+ 
     outfn = "trec_op_" + str(qid)
     outf = open(outfn, "w")
 
-    inUrl = inUrlBeg + query + inUrlEnd
+    inUrl = inUrlBeg + urllib.quote(query) + inUrlEnd
     print inUrl
-    
+  
     data = urllib2.urlopen(inUrl)
 
     print "ran query " + qid
