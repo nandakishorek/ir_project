700f10c96e32f59498ce2953f1055f29e6200900
Nandakishore K <nandakishorek17@gmail.com>
2015-11-15 12:27:06 -0500

    added phrase field and sloppy query phrase matching

--- trec/json_to_trec_custom.py
+++ trec/json_to_trec_custom.py
@@ -64,8 +64,8 @@ for line in queryFile:
     #    if langs == supported_langs[i]:
     #        boost[i] = 2
     #print boost
-    #qf="&qf=text_en^" + str(boost[0]) + "+text_de^" + str(boost[1]) + "+text_ru^" + str(boost[2])
-    qf="&qf=text_en+text_de+text_ru"
+    #qf="&qf=text_en^" + str(boost[0]) + "+text_de^" + str(boost[1]) + "+text_ru^" + str(boost[2]) + "&pf=text_en^" + str(boost[0]) + "+text_ru" + str(boost[1]) + "+text_de^" + str(boost[2]) + "&ps=3"
+    qf="&qf=text_en+text_de+text_ru&pf=text_en+text_ru+text_de&ps=3"
 
     inUrl = inUrlBeg + urllib.quote(query) + inUrlEnd + qf
     print inUrl
